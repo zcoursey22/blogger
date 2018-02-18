a69c81a3de8d0adeeb45a543fcdd1b7a9be9cfa8
Zach Coursey <zcoursey22@gmail.com>
2018-02-17 19:28:15 -0500

    Implement article viewing

--- db/migrate/20180217224508_create_articles.rb
+++ db/migrate/20180217224508_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
-      t.string title
-      t.text body
+      t.string :title
+      t.text :body
 
       t.timestamps
     end
