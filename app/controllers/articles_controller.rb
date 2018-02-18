eae09d4fb930c9e4912ad1cbc32d081dd60d63f0
Zach Coursey <zcoursey22@gmail.com>
2018-02-18 00:01:56 -0500

    Users can edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,13 +17,26 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
   end
 
 end
